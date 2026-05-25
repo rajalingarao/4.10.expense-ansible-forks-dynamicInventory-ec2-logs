3b2ba9560328d8bc61b01751de5b1a403b3741a2
rajalingarao4545 <rajalingarao4545@gmail.com>
2026-05-25 12:45:55 +0530

    expense ansible forks dynamicInventory ec2 logs

--- twelve-ec2-instances/provider.tf
+++ twelve-ec2-instances/provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.48.0"
+      version = ">= 6.37.0"
     }
   }
 
 backend "s3" {
   bucket = "linga-devops-rs"
-  key = "three-ec2-instances-roles-common"
+  key = "three-ec2-instances-ansible-forks-dynamicInv-logs"
   region = "us-east-1"
   #dynamodb_table = "roboshop13-locking"
   use_lockfile = true
